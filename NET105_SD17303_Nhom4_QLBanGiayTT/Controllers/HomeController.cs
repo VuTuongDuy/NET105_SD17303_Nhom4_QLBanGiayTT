4e64a5bff3dd26e007ced87d3be0d0081d4c3d2b
mongbien <duyvtph24890@fpt.edu.vn>
2023-05-12 16:02:50 +0700

    Add project files.

---
+++ NET105_SD17303_Nhom4_QLBanGiayTT/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NET105_SD17303_Nhom4_QLBanGiayTT.Models;
+using System.Diagnostics;
+
+namespace NET105_SD17303_Nhom4_QLBanGiayTT.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NET105_SD17303_Nhom4_QLBanGiayTT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NET105_SD17303_Nhom4_QLBanGiayTT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
